6ca63b5be20fd53d260b4da6a9a0cb1fe0fffd7f
xxx871 <0kh38c317264.7u@gmail.com>
2024-06-11 05:54:50 +0900

    コールバック関数でのユーザー情報保存

--- features/auth/api/authOptions.ts
+++ features/auth/api/authOptions.ts
@@ -1,6 +1,8 @@
+import { axiosInstance } from "@/lib/axios";
 import { NextAuthOptions } from "next-auth";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import { cookies } from "next/headers";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -14,5 +16,42 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
+    async signIn({ user, profile }) {
+      try {
+        const response = await axiosInstance.post(
+          "/auth/oauth",
+          { user, profile },
+          { headers: {
+            "Content-Type": "application/json",
+          },
+          }
+        );
+
+        if (response.status === 200) {
+          const uid = response.headers['uid'];
+          const client = response.headers['client'];
+          const token = response.headers['access-token'];
+          
+          if (uid && client && token) {
+            cookies().set('uid', uid);
+            cookies().set('client', client);
+            cookies().set('access-token', token);
+            return true;
+          } else {
+            console.error("Failed to retrieve tokens from response headers");
+            return false;
+          }
+        } else {
+          console.error("Failed log in");
+          return false;
+        }
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+    async redirect({ baseUrl }) {
+      return baseUrl;
+    },
   },
 };
