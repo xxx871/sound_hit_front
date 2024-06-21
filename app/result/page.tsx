33a20169846434f9715ba3a94c8a761996e64096
xxx871 <0kh38c317264.7u@gmail.com>
2024-06-21 17:30:57 +0900

    ゲーム結果のSNS共有機能実装

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,14 +1,20 @@
 import ResultContent from '@/features/game/components/ResultContent';
+import { getDifficulties } from '@/lib/api/getDifficulties';
+import { getModes } from '@/lib/api/getModes';
 import { getProfile } from '@/lib/api/getProfile'
 import React from 'react'
 
 const Result = async () => {
   const userInfo = await getProfile();
+  const modes = await getModes();
+  const difficulties = await getDifficulties();
 
   return (
     <div>
       <ResultContent
         userInfo={userInfo}
+        modes={modes}
+        difficulties={difficulties}
       />
     </div>
   )
