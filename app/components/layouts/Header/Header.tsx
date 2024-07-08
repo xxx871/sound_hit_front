528edb93a31b6327a8d720ac8daf9d5579161d25
xxx871 <0kh38c317264.7u@gmail.com>
2024-07-08 15:23:37 +0900

    ヘッダーアイコンの調整

--- app/components/layouts/Header/Header.tsx
+++ app/components/layouts/Header/Header.tsx
@@ -3,6 +3,7 @@ import { getUserSession } from '@/lib/session';
 import Link from 'next/link'
 import React from 'react'
 import ModalTutorial from './ModalTutorial';
+import Image from 'next/image';
 
 const Header = async () => {
   const userSession = await getUserSession();
@@ -12,7 +13,13 @@ const Header = async () => {
     <div className="divide-y border-gray-200 dark:border-gray-800 border-b bg-blue-900 h-16 flex items-center">
       <div className="px-4 md:py-2 lg:px-6 w-full">
         <div className="flex items-center justify-between space-y-2 md:space-y-0 md:space-x-6 text-white">
-          <Link href="/" className="float-left text-3xl font-bold tracking-tighter mr-4 border-2 p-2 rounded-full font-palettemosaic">
+          <Link href="/" className="text-3xl font-bold font-palettemosaic flex items-center">
+            <Image
+              src="/header_icon.png"
+              alt="Header Icon"
+              height={50}
+              width={50}
+            />
             おんぴしゃ
           </Link>
           <nav className="flex justify-end items-center space-x-6 text-2xl">
